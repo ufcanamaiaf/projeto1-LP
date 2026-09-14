3843d6e3520d776d6ea189fed90c7109b4f817cb
anazmaiaf <anaz.maiaf@gmail.com>
2026-09-14 18:29:52 -0300

    ajustes de tempo

--- main.c
+++ main.c
@@ -1,20 +1,22 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 
 void d6(){
     srand(time(NULL));
     int d6 = (rand() % 6) + 1;
     int res;
 }
+
 /*
+
 int turno(){
 }
 
 void luta(){
-
 }
+
 typedef struct agente(){
     int vida = 20;
     int dano = 2;
@@ -29,13 +31,13 @@ typedef struct zumbi de sangue() {
     int dano = 4;
     int vida = 18;
     // int def = 0;
-
 }*/
 
 void delay(int seconds){
     sleep(seconds);
 }
-//nessa função, para colocar um intervalo de tempo em segundos entre a execução de um texto/função e outro, como por exemplo 1 segundo, precisa escrever 'delay(1)'
+
+// nessa funcao, para colocar um intervalo de tempo em segundos entre a execucao de um texto/funcao e outro, como por exemplo 1 segundo, precisa escrever 'delay(1)'
 
 char* imprimirTracos(){
     char* tracos = "\n----------------------------------------------------------------------\n";
@@ -43,108 +45,110 @@ char* imprimirTracos(){
 }
 
 char* imprimirCreditos(){
-    char* creditos = "Esse trabalho foi realizado em conjunto por:\n Ana Leticia\nCalebe\nDavi\nKeven\nMarcos\nSannayra\n\nObrigada pela atenção!";
+    char* creditos = "Esse trabalho foi realizado em conjunto por:\n Ana Leticia\nCalebe\nDavi\nKeven\nMarcos\nSannayra\n\nObrigada pela atencao!";
     return creditos;
 }
 
 int main(){
-    printf("\nVocê acorda no meio de uma floresta.\nEsté frio. Não sabe há quanto tempo está ali nem como chegou aquele lugar.\n");
-    printf("\não verificar seus bolsos, encontra alguns objetos:\n");
+
+    printf("\nVoce acorda no meio de uma floresta.\nEste frio. Nao sabe ha quanto tempo esta ali nem como chegou aquele lugar.\n");
+    printf("\nAo verificar seus bolsos, encontra alguns objetos:\n");
 
     printf("%s\n", imprimirTracos());
 
-    // INÍCIO DAS DECISÕES: VER ITENS
+    // INICIO DAS DECISOES: VER ITENS
+
     int decisao1;
+
     while(decisao1 != 6){
 
-    printf("1- Ver arma\n");
-    printf("2- Ver radio\n");
-    printf("3- Ver simbolo da ordem\n");
-    printf("4- Ver fotografia\n");
-    printf("5- Ver lanterna\n");
-    printf("6- Proxima acao\n");
+        printf("1- Ver arma\n");
+        printf("2- Ver radio\n");
+        printf("3- Ver simbolo da ordem\n");
+        printf("4- Ver fotografia\n");
+        printf("5- Ver lanterna\n");
+        printf("6- Proxima acao\n");
+        printf("\nEscolha: ");
 
-    printf("\nEscolha: ");
-    scanf("%d", &decisao1);
+        scanf("%d", &decisao1);
 
-    if(decisao1 == 1){
-        // ver arma
-        printf("\nVocê encontrou a sua arma.\n");
-        delay(1);
-        printf("Uma pistola de acabamento escuro e metal polido, com detalhes discretos no cabo.\n");
-        printf("Mesmo sob a pouca luz, ela parece bem conservada e transmite uma sensacão de confiança.\n\n");
-        delay(5);
+        if(decisao1 == 1){
+            // ver arma
+            printf("\nVoce encontrou a sua arma.\n");
+            printf("Uma pistola de acabamento escuro e metal polido, com detalhes discretos no cabo.\n");
+            printf("Mesmo sob a pouca luz, ela parece bem conservada e transmite uma sensacao de confianca.\n\n");
 
-        printf("%s\n", imprimirTracos());
+            delay(3);
+            printf("%s\n", imprimirTracos());
 
-    } else if(decisao1 == 2){
-        // ver radio
-        printf("\nVocê encontrou um rádio antigo.\n");
-        delay(1);
-        printf("Apesar de estar bastante desgastado, ele ainda parece funcionar.\n");
-        printf("Ao tentar sintonizá-lo, voce escuta uma voz distante...\n\n");
-        delay(4);
-        printf("- Agente... se estiver ouvindo isso, não entre na casa.");
-        delay(2);
-        printf("\n - Você entendeu? NÃO ENTRE NA-\n");
-        delay(1);
-        printf("O radio corta, e você não ouve mais nada.");
-        delay(3);
+        } else if(decisao1 == 2){
+            // ver radio
+            printf("\nVoce encontrou um radio antigo.\n");
+            printf("Apesar de estar bastante desgastado, ele ainda parece funcionar.\n");
+            printf("Ao tentar sintoniza-lo, voce escuta uma voz distante...\n\n");
 
-        printf("%s\n", imprimirTracos());
+            delay(3);
+            printf("- Agente... se estiver ouvindo isso, nao entre na casa.");
+            delay(2);
+            printf("\n - Voce entendeu? NAO ENTRE NA-\n");
+            delay(1);
+            printf("O radio corta, e voce nao ouve mais nada.");
 
-    } else if(decisao1 == 3){
-        // ver simbolo
-        printf("\nVocê encontrou um símbolo da ordem.\n");
-        delay(2);
-        printf("O desenho parece ter sido feito cuidadosamente em um pedaço de metal.\n");
-        printf("Você não reconhece o símbolo, mas sente que ele pode estar relacionado ao lugar.\n\n");
-        delay(5);
+            delay(3);
+            printf("%s\n", imprimirTracos());
 
-        printf("%s\n", imprimirTracos());
+        } else if(decisao1 == 3){
+            // ver simbolo
+            printf("\nVoce encontrou um simbolo da ordem.\n");
+            printf("O desenho parece ter sido feito cuidadosamente em um pedaco de metal.\n");
+            printf("Voce nao reconhece o simbolo, mas sente que ele pode estar relacionado ao lugar.\n\n");
+            
+            delay(3);
+            printf("%s\n", imprimirTracos());
 
-    } else if(decisao1 == 4){
-        // ver fotografia
-        printf("\nVocê encontrou uma fotografia antiga.\n");
-        printf("A imagem mostra algumas pessoas reunidas em frente a uma casa.\n\n");
-        delay(5);
+        } else if(decisao1 == 4){
+            // ver fotografia
+            printf("\nVoce encontrou uma fotografia antiga.\n");
+            printf("A imagem mostra algumas pessoas reunidas em frente a uma casa.\n\n");
+            printf("Voce nota que um dos rostos esta rasgado, como se alguem tivesse usado uma faca para apagar alguem da foto.\n");
+            printf("No verso, ha uma data quase apagada e uma pequena anotacao:\n\n");
 
-        printf("Voce nota que um dos rostos esta rasgado, como se alguém tivesse usado uma faca para apagar alguém da foto.\n");
-        printf("No verso, há uma data quase apagada e uma pequena anotação:\n\n");
-        delay(6);
-        printf("\"Não deixe que ela saia...\"\n\n");
-        delay(2);
+            delay(3);
+            printf("\"Nao deixe que ela saia...\"\n\n");
 
-        printf("%s\n", imprimirTracos());
+            delay(3);
+            printf("%s\n", imprimirTracos());
 
-    } else if(decisao1 == 5){
-        // ver lanterna
-        printf("\nVocê encontrou uma lanterna.\n");
-        delay(3);
-        printf("Ela está velha e coberta de sujeira, mas ainda possui algumas pilhas.\n");
-        printf("Talvez ela seja útil para explorar as partes mais escuras da floresta.\n\n");
-        delay(5);
+        } else if(decisao1 == 5){
+            // ver lanterna
+            printf("\nVoce encontrou uma lanterna.\n");
+            printf("Ela esta velha e coberta de sujeira, mas ainda possui algumas pilhas.\n");
+            printf("Talvez ela seja util para explorar as partes mais escuras da floresta.\n\n");
+            
+            delay(3);
+            printf("%s\n", imprimirTracos());
 
-        printf("%s\n", imprimirTracos());
+        } else if(decisao1 == 6){
+            // o while vai terminar
 
+            printf("%s\n", imprimirTracos());
+        } else {
+            printf("\nOpcao invalida. Escolha um valor entre 1 e 6.\n\n");
+            delay(2);
+        }
 
-    } else if(decisao1 == 6){
-        // o while vai terminar
-        printf("%s\n", imprimirTracos());
-    } else {
-        printf("\nOpção inválida. Escolha um valor entre 1 e 6.\n\n");
-        delay(3);
     }
-}
 
-    // 1ª ACAO: ENTRAR NA CASA OU FUGIR?
-    printf("Na sua frente, há uma grande casa abandonada.\n");
-    printf("Voce se lembra bem disso: e agora que a sua missão começa.\n");
-    delay(4);
+    // 1a ACAO: ENTRAR NA CASA OU FUGIR?
+
+    printf("Na sua frente, ha uma grande casa abandonada.\n");
+    printf("Voce se lembra bem disso: e agora que a sua missao comeca.\n");
+    delay(2);
+
     int decisao2;
 
     printf("1- Abandonar a floresta\n");
-    printf("2- Analisar o perímetro\n");
+    printf("2- Analisar o perimetro\n");
     printf("3- Entrar na casa\n");
 
     printf("\nEscolha: ");
@@ -152,52 +156,53 @@ int main(){
 
     if(decisao2 == 1){
         // ABANDONAR A FLORESTA
-        printf("\nVocê decide ignorar a casa e seguir pela floresta.\n");
+        printf("\nVoce decide ignorar a casa e seguir pela floresta.\n");
         printf("Talvez continuar andando seja a melhor maneira de encontrar uma saida.\n\n");
-        delay(5);
+        delay(2);
 
-        printf("Você caminha por alguns minutos, depois por horas.\n");
-        printf("As árvores parecem ficar cada vez mais densas, e a luz do sol desaparece entre as copas.\n\n");
-        delay(5);
+        printf("Voce caminha por alguns minutos, depois por horas.\n");
+        printf("As arvores parecem ficar cada vez mais densas, e a luz do sol desaparece entre as copas.\n\n");
+        delay(2);
 
-        printf("Depois de algum tempo, algo chama sua atenção.\n");
-        printf("Aquela árvore... voce já passou por ela antes.\n\n");
-        delay(4);
+        printf("Depois de algum tempo, algo chama sua atencao.\n");
+        printf("Aquela arvore... voce ja passou por ela antes.\n\n");
+        delay(2);
 
-        printf("Você continua andando, mas a sensação permanece.\n");
-        printf("Nao importa qual caminho escolha, a floresta parece sempre levá-lo para o mesmo lugar.\n\n");
-        delay(6);
+        printf("Voce continua andando, mas a sensacao permanece.\n");
+        printf("Nao importa qual caminho escolha, a floresta parece sempre leva-lo para o mesmo lugar.\n\n");
+        delay(2);
 
-        printf("Após mais algum tempo, você percebe marcas escuras nos troncos das árvores.\n");
-        printf("Ao se aproximar, percebe que são marcas de sangue.\n\n");
-        delay(5);
+        printf("Apos mais algum tempo, voce percebe marcas escuras nos troncos das arvores.\n");
+        printf("Ao se aproximar, percebe que sao marcas de sangue.\n\n");
+        delay(2);
 
-        printf("Poucos metros adiante, há uma mochila abandonada no chão.\n");
-        printf("Pela aparência, ela parece pertencer a equipe que esteve aqui antes de voce.\n\n");
-        delay(5);
+        printf("Poucos metros adiante, ha uma mochila abandonada no chao.\n");
+        printf("Pela aparencia, ela parece pertencer a equipe que esteve aqui antes de voce.\n\n");
+        delay(2);
 
-        printf("Dentro da mochila, você encontra alguns documentos e registros.\n");
-        printf("Um deles chama sua atencão.\n\n");
-        delay(5);
+        printf("Dentro da mochila, voce encontra alguns documentos e registros.\n");
+        printf("Um deles chama sua atencao.\n\n");
+        delay(2);
 
-        printf("\"A entidade não está presa na casa.\"\n\n");
-        delay(3);
+        printf("\"A entidade nao esta presa na casa.\"\n\n");
+        delay(2);
 
         printf("Voce mal consegue terminar de ler.\n");
-        printf("Um barulho vem de dentro da floresta.\n\n");
-        delay(4);
+        printf("Um barulho vem de dentro da floresta...\n\n");
+
+        delay(5);
 
         printf("Galhos quebram. Folhas se movimentam.\n");
-        printf("Você se vira lentamente na direção do som.\n\n");
+        printf("Voce se vira lentamente na direcao do som.\n\n");
+        delay(2);
+
+        printf("Uma criatura coberta de sangue surge entre as arvores. Voce ja lutou com algo parecido...\n");
+        printf("Um Zumbi de Sangue.\n");
+        printf("Ela permanece parada por alguns segundos, olhando diretamente para voce.\n\n");
         delay(4);
 
-        printf("Uma criatura coberta de sangue surge entre as árvores. Você já lutou com algo parecido...\n");
-        printf("Um Zumbi de Sangue.\n");
-        printf("Ela permanece parada por alguns segundos, olhando diretamente para você.\n\n");
-        delay(7);
-
-        printf("Essa é a sua primeira batalha...\n\n");
-        delay(3);
+        printf("Essa e a sua primeira batalha...\n\n");
+        delay(2);
 
         printf("%s\n", imprimirTracos());
 
@@ -206,13 +211,12 @@ int main(){
         // 1.1: LUTAR OU FUGIR
         printf("1- Lutar\n");
         printf("2- Fugir\n");
-
         printf("\nEscolha: ");
+
         scanf("%d", &decisao2a);
 
         if(decisao2a == 1){
-            // LUTAR COM O ZUMBI (fazer sistema de luta)
-
+            // LUTAR COM O ZUMBI 
             printf("Voce luta e vence");
             delay(2);
 
@@ -222,43 +226,48 @@ int main(){
             // FUGIR E MORRER
             printf("Desesperado e com medo, voce faz de tudo para fugir da criatura\n");
             printf("Voce tenta retornar a casa, mas a floresta fica cada vez mais sufocante.\n\n");
-            delay(7);
+
+            delay(2);
 
             printf("E tarde demais, voce deixou tudo para tras. Sua casa, sua familia, e voce sabe o que vai acontecer.\n");
             printf("Voce vai morrer aqui\n\n");
-            delay(7);
+            delay(3);
 
-            printf("O seu rádio comeca a transmitir uma voz angelical, doce e sutil\n");
+            printf("O seu radio comeca a transmitir uma voz angelical, doce e sutil\n");
             printf("E uma crianca, e ela te diz:\n");
-            delay(5);
 
+            delay(4);
             printf("- Voce nao deveria ter vindo.\n\n");
             delay(2);
 
-            printf("As arvores voltam a mexer violentamente atrás de você, você percebe que não está mais sozinho. \n");
-            printf("Assim como você percebeu que o caminho que escolheu no ínicio acabou de levá-lo a morte.\n\n");
-            delay(7);
+            printf("As arvores voltam a mexer violentamente atras de voce, voce percebe que nao esta mais sozinho. \n");
+            printf("Assim como voce percebeu que o caminho que escolheu no inicio acabou de leva-lo a morte.\n\n");
+            delay(5);
 
-            printf("O Zumbi de Sangue te alcanca.");
+            printf("O Zumbi de Sangue te alcanca.\n\n");
             delay(2);
+
+            printf("Voce esta morto, agente.");
 
             printf("%s\n", imprimirTracos());
 
-
+            delay(5);
         } else {
-            printf("\nOpção inválida. Escolha um valor entre 1 e 2\n");
-            delay(3);
+            printf("\nOpcao invalida. Escolha um valor entre 1 e 2\n");
         }
 
     } else if(decisao2 == 2){
+
         // ANALISAR O PERIMETRO
 
     } else if(decisao2 == 3){
+
         // ENTRAR NA CASA
 
     } else {
-        printf("\nOpção inválida. Escolha um valor entre 1 e 3\n");
+        printf("\nOpcao invalida. Escolha um valor entre 1 e 3\n");
     }
 
     return 0;
+
 }
